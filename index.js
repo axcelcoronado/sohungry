ba308235060b3eb2e2823f4b3d8a2e6b4b587d82
axcelcorona <axcelcorona01@gmail.com>
2020-12-17 01:01:55 -0500

    el servidor ya esta creado

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 import express from 'express';
-
+import morgan from 'morgan';
+import cors from 'cors';
+import path from 'path'
+import mongoose from 'mongoose';
 
 const app = express();
 
 
 
-app.listen(3000, ()=>{
+
+//middle ware on express
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('port', process.env.PORT|| 3000);
+
+app.listen(app.get('port'), ()=>{
     console.log('server on port 3000');
 })
